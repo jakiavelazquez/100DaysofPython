de300945e0c674caf1e61905804fd62e35540136
jakiavelazquez <jakiadavis84@gmail.com>
2023-11-04 22:38:48 +0000

    odd or even

---
+++ Day3/odd_or_even.py
@@ -0,0 +1,11 @@
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+# If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+  print("This is an even number.")
+# Otherwise (number cannot be divided by 2 with 0 remainder).
+else: 
+  print("This is an odd number.")
